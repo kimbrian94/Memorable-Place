32d49ee0c8670a561eae59b0970bac1b11f36053
Brian Kim <kim.brian94@gmail.com>
2020-07-03 22:56:40 -0400

    Initial Commit

---
+++ Memorable Places 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorable Places 2
+//
+//  Created by Brian Kim on 2020-07-03.
+//  Copyright © 2020 Brian Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
